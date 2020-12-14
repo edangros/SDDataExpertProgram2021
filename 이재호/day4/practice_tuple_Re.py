15e5f05c00cccc8b51e2e2533029dbbbde342428
AraLab <edangros@naver.com>
2020-12-14 09:46:39 +0900

    lambda added

--- 이재호/day4/practice_tuple_Re.py
+++ 이재호/day4/practice_tuple_Re.py
@@ -16,7 +16,14 @@ with open('이재호/day3/mbox.txt') as fhand:
         addresses = re.findall('[A-Za-z0-9.]\S*@\S*[A-Za-z0-9.]',line)
         for address in addresses:
             mails[address] = mails.get(address,0)+1
-            print (address)
-        
+# 얻은 메일 주소를 집어넣자
+with open('이재호/day4/tupleoutput.txt', 'w') as fhand:
+    # 먼저 set을 통해 중복되는거 다 제거
+    adset = set(mails)
+    for s in adset:
+        fhand.write(s)
+
 # 리스트 컴프리헨션 => k,v를 mail의 모든 항목에 대해 이터레이션하며 (v,k)튜플로 입력, 내림차순 정렬 후 상위 5개만 추출
-print(sorted([(v,k) for (k,v) in mails.items()],reverse=True)[:5])
+# 람다까지 결합해서 다시 뒤집어줌
+switch= lambda item: (item[1],item[0]) #이렇게 정의된 함수는 '이터레이터'(반복자)로 작용 가능
+print(list(map(switch ,sorted([(v,k) for (k,v) in mails.items()],reverse=True)[:5])))
